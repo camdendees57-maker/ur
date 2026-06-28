3958302f9b38fb622e4a3060d42f730d948c0ec2
camdendees57-maker <camdendees57@gmail.com>
2026-06-28 01:52:13 -0500

    Delete jni directory

--- jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := moneyhack
-LOCAL_SRC_FILES := main.cpp
-LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2
-LOCAL_CFLAGS := -fvisibility=hidden
-LOCAL_CPPFLAGS := -std=c++17 -fvisibility=hidden
-include $(BUILD_SHARED_LIBRARY)
--- jni/Application.mk
+++
@@ -1,7 +0,0 @@
-APP_ABI := arm64-v8a
-APP_PLATFORM := android-21
-APP_STL := c++_static
-APP_OPTIM := release
-APP_CFLAGS := -fvisibility=hidden -fvisibility-inlines-hidden
-APP_CPPFLAGS := -std=c++17 -fvisibility=hidden -fvisibility-inlines-hidden
-NDK_TOOLCHAIN_VERSION := clang
